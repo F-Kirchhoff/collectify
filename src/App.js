49b73dfb5912485d4a124906ceaa8a7f950a0874
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2023-11-07 15:59:27 +0100

    setup app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { data } from "./db";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Collectify</h1>;
+      <ul>
+        {data.map((album) => (
+          <li>{album.title}</li>
+        ))}
+      </ul>
+    </main>
   );
 }
 
