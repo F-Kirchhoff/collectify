38f08b9fadd0d56b9960e8b83a0634b329ddb8f0
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2023-11-07 16:38:44 +0100

    feat: add songlist display

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -3,11 +3,22 @@ import "./AlbumCard.css";
 export default function AlbumCard({ title, artist, image }) {
   return (
     <section className="album-card">
-      <img src={image.url} alt={title} className="album-card__image" />
       <div className="album-card__info">
+        <img src={image.url} alt={title} className="album-card__image" />
         <h2>{title}</h2>
         <small>{artist}</small>
       </div>
+      <ul className="album-card__songlist">
+        {Array(10)
+          .fill({ name: "test" })
+          .map((song, index) => (
+            <li key={song.name} className="album-card__song">
+              <span>{index + 1}.</span>
+              <span>{song.name}</span>
+              <span>03:55</span>
+            </li>
+          ))}
+      </ul>
     </section>
   );
 }
