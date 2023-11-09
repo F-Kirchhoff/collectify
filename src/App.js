c53927e653b45ceaefe16ae2785b02ae79d6e2a8
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2023-11-09 14:48:49 +0100

    feat: fetch featured albums

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import AlbumCard from "./components/AlbumCard";
 import { data } from "./db";
 
 function App() {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:3000/api/featured");
+        const albums = await response.json();
+        setAlbums(albums);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <main class="main">
       <h1>Collectify</h1>
       <ul className="album-list">
-        {data.map((album) => (
+        {albums.map((album) => (
           <li key={album.id}>
             <AlbumCard {...album} />
           </li>
