2e2f4982b376473a9182cfe43c5c720f9bb23235
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2023-11-22 11:10:03 +0100

    fix: remove pages folder

--- src/pages/Favorites.js
+++
@@ -1,7 +0,0 @@
-import AlbumList from "../components/AlbumList";
-
-export default function Favorites({ albums, onToggleSave }) {
-  return (
-    <AlbumList list={albums} title="Saved Albums" onToggleSave={onToggleSave} />
-  );
-}
